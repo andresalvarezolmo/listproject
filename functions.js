1fc55c8aaf8d3cc4ba985189f93ad9c1e0a90070
Andres <xxdrews@gmail.com>
2020-04-18 12:19:39 +0200

    alerts replaced by inline texts

--- functions.js
+++ functions.js
@@ -29,7 +29,8 @@ function addToList()
     if (filling == null ||
         filling == undefined ||
         filling.length == 0  ) {
-        alert("Please write something before adding it to the list");
+        document.getElementById("addText").innerHTML = "Please write something before adding it to the list";
+        $("#addText").css("display", "block")
         return -1;
     }
     var newNode = document.createElement("li");
@@ -39,6 +40,7 @@ function addToList()
     newNode.appendChild(content);
     var element = document.getElementById("olist");
     element.appendChild(newNode);
+    $("#addText").css("display", "none")
     document.getElementById("tasklist").value = "";
 }
 
@@ -50,7 +52,8 @@ function deleteFromList()
     //store tasks in an array to make validations later
     var lis = document.getElementsByTagName("li");
     if (lis.length == 0) {
-        alert("There are no to-dos to delete");
+        document.getElementById("deleteText").innerHTML = "There are no to-dos to delete";
+        $("#deleteText").css("display", "block")
         return -1;
     }
 
@@ -59,7 +62,9 @@ function deleteFromList()
         deleteFilling == undefined ||
         deleteFilling.length == 0  ) 
     {
-        alert("Please write something before removing it from the list");
+        document.getElementById("deleteText").innerHTML = "Please write something before removing it from the list";
+        $("#deleteText").css("display", "block")
+        // alert("Please write something before removing it from the list");
         return -1;
     }
 
@@ -72,7 +77,8 @@ function deleteFromList()
         }
         else{
             document.getElementById("deleteItem").value = "";
-            alert("Please enter an existent task to delete");         
+            document.getElementById("deleteText").innerHTML = "Please enter an existent task to delete";
+            $("#deleteText").css("display", "block")
             return -1;
         }
     }
@@ -80,23 +86,6 @@ function deleteFromList()
     //delete node with selected task
     var node = document.getElementById(deleteFilling);
     node.parentNode.removeChild(node);
+    $("#deleteText").css("display", "none")
     document.getElementById("deleteItem").value = "";
-}
-
-function searchList(){
-    var deleteFilling = document.getElementById("deleteItem").value;
-    var index = 0;
-    var found = false;
-    var lis = document.getElementsByTagName("li");
-    for (let index = 0; index < lis.length; index++) {
-        // console.log(lis[index].textContent);
-        if (lis[index].textContent == deleteFilling) {
-            console.log("Coincidence found!");
-            found = true;
-        }
-        else{
-            alert("Pleas enter an existent task to delete");         
-            return -1;
-        }
-    }
 }
